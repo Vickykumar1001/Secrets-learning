53e579ae6e03c398f7492751cc6463c55c5868d2
Vicky <vicky9060993097@gmail.com>
2023-02-17 16:57:48 +0530

    memory store

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const MemoryStore = require('memorystore')(session)
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
@@ -16,10 +17,10 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('trust proxy', 1);
 app.use(session({
-  cookie:{
-    secure: true,
-    maxAge:60000
-       },
+  cookie: { maxAge: 86400000 },
+    store: new MemoryStore({
+      checkPeriod: 86400000 // prune expired entries every 24h
+    }),
     secret: "This is my Secret",
     resave: false,
     saveUninitialized: false,
